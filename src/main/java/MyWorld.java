a5c94b0f8c02aedf80b239fea5c40dde8a9f5b40
nirmalmandhata <nirmalmandhata@gmail.com>
2018-06-06 17:02:42 +0530

    Fix #2 ~inprogress

--- src/main/java/MyWorld.java
+++ src/main/java/MyWorld.java
@@ -3,8 +3,8 @@ class MyWorld{
 	public static void main(String arg[]) {
 		String message = welcomeMessage();
 		System.out.println("\n\n"+message);
-		System.out.println("success #1 updates-progress - issue reporting");
-
+		System.out.println("success #1 updates-progress -- issue reporting");
+-
 	}
 	
 	public static String welcomeMessage() {
