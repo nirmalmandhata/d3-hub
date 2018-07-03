d3d782e02988b8895b8d53d43999507435761362
nirmalmandhata <nirmalmandhata@gmail.com>
2018-07-03 11:13:24 +0530

    Update MyWorld.java
    commit

--- src/main/java/MyWorld.java
+++ src/main/java/MyWorld.java
@@ -3,7 +3,7 @@ class MyWorld{
 	public static void main(String arg[]) {
 		String message = welcomeMessage();
 		System.out.println("\n\n------"+message);
-		System.out.println(" changes done - success #1 updates-progress-done issue reporting --m sdsds");
+		System.out.println(" changes done - success #1 updates-progress-done issue reporting   test--m sdsds");
 
 	}
 	
